05cf9a63d7cf2b80bf18a29b41c7981553b4da89
theyanagt <97217373+theyanagt@users.noreply.github.com>
2022-01-06 15:24:04 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
-pkg update
-pkg install nodejs
-pkg install libwebp
-pkg install ffmpeg
-pkg install wget
-pkg install tesseract
+#!/usr/bin/bash
+
+pkg clean
+pkg install tty-clock -y
+pkg install sox -y
+pkg install jq -y
+pkg install figlet -y
+pkg install wget -y
+pkg install ffmpeg libwebp -y
+pkg install nodejs -y
+pkg install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-cd node_modules/@adiwajshing/baileys-md
-npm i typescript -g
-tsc
-cd ../../../
+npm audit fix
+
+#!/usr/bin/bash
+
+tty-clock -s -S -x -c -C 2 -b -B
+clear
+node index
